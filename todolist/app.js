698f195606b19eb000abdf8f11f7f9242810f2dc
DanyelleH <danyellehenriquez@gmail.com>
2024-09-17 19:12:20 -0400

    functionally correct     modified:   todolist/app.js     modified:   todolist/todolist.html

--- todolist/app.js
+++ todolist/app.js
@@ -1,11 +1,43 @@
 document.querySelector("#AddTask").addEventListener("click",addEvent);
 function addEvent() {
     let addedTask = document.querySelector("#input").value;
-    // list.textContent = addedTasktask;
-    // document.getElementById("toDoList").appendChild(list);
-    // localStorage.setItem("task", [])
+
     let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
     tasks.push(addedTask)
     
     localStorage.setItem("tasks", JSON.stringify(tasks));
+    document.querySelector("#input").value = "";
+    updateDisplay()
 }
+
+
+function updateDisplay() {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let taskList = document.getElementById("toDoList");
+    taskList.innerHTML =""
+
+    tasks.forEach((task, index) => {
+        let listItem= document.createElement("li");
+        listItem.textContent = task ;
+
+        let removeButton = document.createElement("button");
+        removeButton.textContent= "Remove";
+        removeButton.addEventListener("click", function(){
+            removeTask(index);
+    });
+    listItem.appendChild(removeButton)
+    taskList.appendChild(listItem)
+    })
+}
+
+function removeTask(index) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.splice(index,1);
+     localStorage.setItem("tasks", JSON.stringify(tasks));
+     updateDisplay()
+}
+updateDisplay()
+
+
+// document.querySelector("#toDoList").innerHTML = (localStorage.getItem("tasks"))
