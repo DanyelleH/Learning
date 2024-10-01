913055d9b01fdf13fe15a6489d2fb6103dc742c5
DanyelleH <danyellehenriquez@gmail.com>
2024-10-01 18:43:31 -0400

    Complexity is complex 	modified:   .DS_Store 	new file:   BigO ProblemSolving/.DS_Store 	new file:   BigO ProblemSolving/README.md 	new file:   BigO ProblemSolving/anagrams/README.md 	new file:   BigO ProblemSolving/anagrams/anagrams.py 	new file:   BigO ProblemSolving/bigOClassExamples/bigOexample.py 	new file:   BigO ProblemSolving/frequencyCounter/README.md 	new file:   BigO ProblemSolving/frequencyCounter/frequencycounter.py 	new file:   BigO ProblemSolving/multiplePointers/README.md 	new file:   BigO ProblemSolving/multiplePointers/multiplepointers.py 	new file:   BigO ProblemSolving/slidingWindow/README.md 	new file:   BigO ProblemSolving/slidingWindow/slidingwindow.py 	new file:   BigO ProblemSolving/uniqueValues/README.md 	new file:   BigO ProblemSolving/uniqueValues/uniquevalues.py

---
+++ BigO ProblemSolving/bigOClassExamples/bigOexample.py
@@ -0,0 +1,8 @@
+def count(num): #nested
+    for i in range (num):
+        for j in range(num):
+            print(i,j)
+count(20)
+
+# non nester loops -> do not increase complexity.
+#Nested loops increase complexity.
